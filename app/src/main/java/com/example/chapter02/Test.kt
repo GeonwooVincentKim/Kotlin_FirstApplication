9dfce240cfe8cf5b449ce626079f0b0632520f80
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-07 13:06:45 +0900

    [Feature] - No errors in `Test.kt`, but cannot execute the file

---
+++ app/src/main/java/com/example/chapter02/Test.kt
@@ -0,0 +1,39 @@
+package com.example.chapter02
+
+import kotlinx.coroutines.*
+
+fun main(){
+    val scope = GlobalScope
+
+    runBlocking{
+        val a = launch{
+            for(i in 1..5){
+                println(i)
+                delay(10)
+            }
+        }
+
+        val b = async {
+            "async 종료"
+        }
+
+        println("async 대기")
+        println(b.await())
+
+        println("launch 취소")
+        a.cancel()
+        println("launch 종료")
+    }
+
+    runBlocking {
+        var result = withTimeoutOrNull(50){
+            for(i in 1..10){
+                println(i)
+                delay(10)
+            }
+            "Finish"
+        }
+
+        println(result)
+    }
+}
